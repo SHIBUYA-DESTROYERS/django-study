d91bd001dfdcbb0b6e2c168e1e3d34a6ca4e5a27
raryosu <me@raryosu.info>
2017-10-09 22:59:20 +0900

    一覧のviewを作成

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from cms.models import Book
+
 def book_list(request):
-    return HttpResponse('書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render(request,
+                  'cms/book_list.html',
+                  {'books': books})
 
 def book_edit(request):
     return HttpResponse('書籍の編集')
