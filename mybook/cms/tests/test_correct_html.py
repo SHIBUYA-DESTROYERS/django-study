483e639c427c93daae37ef6ea48472ab7e4babe6
raryosu <me@raryosu.info>
2017-10-10 16:50:55 +0900

    Add tests

---
+++ mybook/cms/tests/test_correct_html.py
@@ -0,0 +1,12 @@
+from django.http import HttpRequest
+from django.templete.loader import render_to_string
+from django.test import TestCase
+from cms.views import book_list
+
+class HtmlTests(TestCase):
+    def test_book_list_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = book_list(request)
+        expected_html = render_to_string('cms/book_list.html',
+                {books: []})
+        self.assertEqual(response.content.decode(), expected_html)
